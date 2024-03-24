28092efa2a0c049f1aed88cdeacf257c4892d7ea
mercenarioz <funnystar320@gmail.com>
2024-03-24 14:25:15 +0700

    feat: server script

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, send_file, render_template
+from flask import Flask, request, send_file, render_template, render_template_string
+from script import remove_background
 from io import BytesIO
-import subprocess
-from PIL import Image
-from rembg import remove
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -12,18 +11,34 @@ def image_process():
     if request.method == "POST":
         if "image_file" not in request.files:
             return "No file uploaded", 400
+
         image_file = request.files["image_file"]
+
         if image_file.filename == "":
             return "No file selected", 400
+
         if image_file:
-            input_image = Image.open(image_file.stream)
-            output_image = remove(input_image)
-            img_io = BytesIO()
-            output_image.save(img_io, 'PNG')
-            img_io.seek(0)
-            return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='bg-removed.png')
+            # get the extension of the file
+            filename = secure_filename(image_file.filename)
+            file_extension = filename.rsplit(
+                '.', 1)[1].lower() if '.' in filename else ''
+
+            allowed_extensions = {'jpg', 'jpeg', 'png'}
+            # check if the file is allowed
+            if file_extension not in allowed_extensions:
+                return render_template_string('''
+                    <h1>Invalid file extension</h1>
+                    <p>Only .jpg, .jpeg, .png files are allowed</p>
+                    <a href="/">Go back</a>
+                '''), 400
+
+            try:
+                processed_img = remove_background(image_file)
+                return send_file(BytesIO(processed_img), mimetype="image/png", as_attachment=True, download_name="processed_image.png")
+            except Exception as e:
+                return str(e), 500
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5433)
--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 from rembg import remove
 from PIL import Image
+from io import BytesIO
 
 
-def remove_background(image_path):
+def remove_background(image_file):
     # Load image
-    img = Image.open(image_path)
+    img = Image.open(image_file.stream)
     img = img.convert("RGBA")  # Convert image to RGBA mode
 
     # Remove background
@@ -14,25 +15,9 @@ def remove_background(image_path):
     white_bg = Image.new("RGBA", processed_img.size, (255, 255, 255))
     with_white_background = Image.alpha_composite(
         white_bg, processed_img.convert("RGBA"))
+    img_io = BytesIO()
+    with_white_background.save(img_io, "PNG")
+    img_io.seek(0)
 
-    # Save processed image to a file
-    # processed_img.save('./processed_image.png')
-
-    # Save image with white background to a file
-    with_white_background.save('./img.png')
-
-    # _, axes = plt.subplots(1, 2, figsize=(10, 5))
-    # axes[0].imshow(img)
-    # axes[0].set_title('Original Image')
-    # axes[0].axis('off')
-
-    # axes[1].imshow(processed_img)
-    # axes[1].set_title('Background Removed')
-    # axes[1].axis('off')
-
-    # plt.show()
-
-
-if __name__ == "__main__":
-    image_path = './pre-grape.jpg'
-    remove_background(image_path)
+    # return the image file
+    return img_io.getvalue()
